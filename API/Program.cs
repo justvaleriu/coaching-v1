63f8e6296b85d4464fafac9358b61bbffc02c825
Valeriu Sandu <vasandu@deloittece.com>
2025-04-19 12:59:22 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Core.Interfaces;
+using Infrastructure.Config;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,11 +12,26 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
 
 app.Run();
