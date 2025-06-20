81b020678afbf40808c0c7eadd2cd7022a88832d
Valeriu Sandu <vasandu@deloittece.com>
2025-06-20 20:41:28 +0300

    End of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,12 @@
 using API.Middleware;
 using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Config;
 using Infrastructure.Data;
 using Infrastructure.Data.SeedData;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +18,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped (typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config => 
+{
+    var connString = builder.Configuration.GetConnectionString("Redis") 
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
@@ -35,9 +46,9 @@ try
     await context.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
 }
-catch (Exception e)
+catch (Exception ex)
 {
-    Console.WriteLine(e);
+    Console.WriteLine(ex);
     throw;
 }
 
