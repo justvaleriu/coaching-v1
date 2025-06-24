543b81e02636169344bb075e8959cae0c5c17cfc
Valeriu Sandu <vasandu@deloittece.com>
2025-06-24 16:53:14 +0300

    End of section 13

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,15 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
+
+function initializeApp() {
+  const initService = inject(InitService); 
+  return lastValueFrom(initService.init()).finally(() => {
+    const splash = document.getElementById('initial-splash');
+    if (splash) {
+      splash.remove(); 
+    }
+  });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
-  ]
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(initializeApp), // Provide the initialization function directly
+  ],
 };
