99c37b55606092814b6340e49095fe5d8b18dd7a
Valeriu Sandu <vasandu@deloittece.com>
2025-07-03 11:42:21 +0300

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51Rfm7eD3pgBKygusq83vjmyiJV5V5JgF8Sx1Q7hahQzVQpPjMLOBVUMAfQUQVY0TduNLL62oxmWBr0qcTh6ZLvhD00WvzxG0Hz'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51Rfm7eD3pgBKygusq83vjmyiJV5V5JgF8Sx1Q7hahQzVQpPjMLOBVUMAfQUQVY0TduNLL62oxmWBr0qcTh6ZLvhD00WvzxG0Hz'
 };
